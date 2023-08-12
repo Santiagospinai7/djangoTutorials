62a337362bfce6c52314f8c4356f55276fe77eee
Santiago Ospina Idrobo <santiagospidrobo15@gmail.com>
2023-08-12 14:45:20 -0500

    prducts index and display done

--- 01_Tutorial/helloWorld/pages/views.py
+++ 01_Tutorial/helloWorld/pages/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render # here by default
 from django.http import HttpResponse # new
 from django.views.generic import TemplateView # new
-
-# Create your views here.
-# def homePageView(request): # new
-#     return HttpResponse('Hello World!') # new
+from django.views import View
 
 class HomePageView(TemplateView):
   template_name = 'pages/home.html'
@@ -37,3 +34,35 @@ class ContactPageView(TemplateView):
     })
 
     return context
+
+class Product:
+    products = [
+        {"id":"1", "name":"TV", "description":"Best TV"},
+        {"id":"2", "name":"iPhone", "description":"Best iPhone"},
+        {"id":"3", "name":"Chromecast", "description":"Best Chromecast"},
+        {"id":"4", "name":"Glasses", "description":"Best Glasses"}
+    ]
+
+class ProductIndexView(View):
+    template_name = 'products/index.html'
+
+    def get(self, request):
+        viewData = {}
+        viewData["title"] = "Products - Online Store"
+        viewData["subtitle"] =  "List of products"
+        viewData["products"] = Product.products
+
+        return render(request, self.template_name, viewData)
+
+class ProductShowView(View):
+    template_name = 'products/show.html'
+
+
+    def get(self, request, id):
+        viewData = {}
+        product = Product.products[int(id)-1]
+        viewData["title"] = product["name"] + " - Online Store"
+        viewData["subtitle"] =  product["name"] + " - Product information"
+        viewData["product"] = product
+
+        return render(request, self.template_name, viewData)
