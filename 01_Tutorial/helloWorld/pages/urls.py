c52734fbbd74d1f51acc8f29a728a5479fc1d2ed
Santiago Ospina Idrobo <santiagospidrobo15@gmail.com>
2023-08-26 17:16:49 -0500

    cart systm implemented

--- 01_Tutorial/helloWorld/pages/urls.py
+++ 01_Tutorial/helloWorld/pages/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView, ContactPageView, ProductIndexView, ProductShowView, ProductCreateView, CommentCreateView
+from .views import HomePageView, AboutPageView, ContactPageView, ProductIndexView, ProductShowView, ProductCreateView, CommentCreateView, CartView, CartRemoveAllView
 
 urlpatterns = [
   path("", HomePageView.as_view(), name='home'),
@@ -9,4 +9,7 @@ urlpatterns = [
   path('products/create', ProductCreateView.as_view(), name='form'),
   path('products/<str:id>', ProductShowView.as_view(), name='show'),
   path('products/<str:id>/comment/create', CommentCreateView.as_view(), name='add_comment'),
+  path('cart/', CartView.as_view(), name='cart_index'),
+  path('cart/add/<str:product_id>', CartView.as_view(), name='cart_add'),
+  path('cart/removeAll', CartRemoveAllView.as_view(), name='cart_removeAll'),
 ]
