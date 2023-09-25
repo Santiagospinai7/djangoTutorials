f86edaf366ed9f17eaff258a92ded8e2656b4c16
Santiago Ospina Idrobo <santiagospidrobo15@gmail.com>
2023-09-25 16:53:10 -0500

    add crud operations to the API system

--- todoapp/backend/api/views.py
+++ todoapp/backend/api/views.py
@@ -18,3 +18,11 @@ class TodoListCreate(generics.ListCreateAPIView):
   def perform_create(self, serializer):
     #serializer holds a django model
     serializer.save(user=self.request.user)
+
+class TodoRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+  serializer_class = ToDoSerializer
+  permission_classes = [permissions.IsAuthenticated]
+
+  def get_queryset(self):
+    user = self.request.user
+    return ToDo.objects.filter(user=user)
